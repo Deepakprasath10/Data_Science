141e7d109094070619fae4eda0c1e4687f555846
Deepakprasath10 <deepakprasath02@gmail.com>
2025-02-27 12:01:11 +0530

    modify

--- factorial.java
+++ factorial.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {   
        Scanner sc = new Scanner(System.in);
-       int a = sc.nextInt();
+       int b = sc.nextInt();
       
        int factorial = 1;
-       for(int i = 1;i<=a;i++){
+       for(int i = 1;i<=b;i++){
        factorial *=i;
        
        }
